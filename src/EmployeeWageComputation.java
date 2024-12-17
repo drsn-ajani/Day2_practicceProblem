c039dc01d268bae40f00c77692e950bc640f94f4
drsn-ajani <daajani9103@gmail.com>
2024-12-17 15:19:30 +0530

    UC 3: Added Part Time Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -7,13 +7,27 @@ public class EmployeeWageComputation {
         int EmployeeWagePerHour = 20;
         int full_day_hour = 8;
 
+        int IS_FULL_TIME_PRESENT = 1;
+        int IS_PART_TIME_PRESENT = 2;
+
         double empCheck = Math.floor(Math.random() * 100) % 2;
+        double part_full = Math.floor(Math.random() * 100) % 2;
 
         if (empCheck == IS_EMPLOYEE_PRESENT) {
-            System.out.println("EMPLOYEE IS PRESENT\n");
+            double working_hour = 0;
 
             // UC-2
-            double wage = EmployeeWagePerHour * full_day_hour;
+            // UC-3
+
+            if (part_full == IS_PART_TIME_PRESENT) {
+                System.out.println("EMPLOYEE IS PRESENT FOR PART TIME\n");
+                working_hour = 4;
+            } else {
+                System.out.println("EMPLOYEE IS PRESENT FOR FULL TIME\n");
+                working_hour = 8;
+            }
+
+            double wage = EmployeeWagePerHour * working_hour;
             System.out.println("Employee Wage: " + wage);
         } else {
             System.out.println("EMPLOYEE IS NOT PRESENT\n");
