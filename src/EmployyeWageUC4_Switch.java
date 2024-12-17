cffe71e39397eda5e27962c41659ff19b0f23ff8
drsn-ajani <daajani9103@gmail.com>
2024-12-17 15:36:35 +0530

    UC 4: Implement Employee Wage Using Switch-2

--- src/EmployyeWageUC4_Switch.java
+++ src/EmployyeWageUC4_Switch.java
@@ -1,4 +1,4 @@
-public class EmployeeWageUC4_Switch {
+public class EmployyeWageUC4_Switch {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
